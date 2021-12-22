e8994a1d3c188bab7d530e2d37ffb26835c4397f
Pallagi Joe <pallagijoe@gmail.com>
2021-12-22 17:18:12 +0100

    create stripe account on signup

--- onboarding/functions/src/index.ts
+++ onboarding/functions/src/index.ts
@@ -1,5 +1,4 @@
 import * as functions from "firebase-functions";
-
 import * as admin from "firebase-admin";
 
 admin.initializeApp();
@@ -7,22 +6,33 @@ interface UserModel {
   uid: string
   displayName: string,
   email: string,
-  imagePath : string
+  stripeUid: string,
+  imagePath : string,
 }
 
-exports.createUserInDb = functions.auth.user().onCreate((user) => {
-  const email = user.email;
-  const displayName = user.displayName; 
+const stripe = require('stripe')(functions.config().stripe.testkey)
+  exports.createStripeCustomer = functions.auth.user().onCreate( createdUser => {
+       return stripe.customers.create({email : createdUser.email})
+       .then((stripeUser : any) => {
+        const userModel : UserModel = {
+          displayName: createdUser.displayName ? createdUser.displayName : 'name is missing',
+          email: createdUser.email ? createdUser.email : '',
+          uid: createdUser.uid,
+          stripeUid : stripeUser.id,
+          imagePath : createdUser.photoURL? createdUser.photoURL : 'https://avatars.dicebear.com/api/gridy/' + createdUser.uid + '.svg',
+        }
+        return admin.firestore().collection('accounts').doc(createdUser.uid).set(userModel)
+       }
+       );
+  });
 
-  functions.logger.log('signed up email is ' + email);
-  functions.logger.log('signed up name is ' + displayName);
+// exports.createSubscription = functions.database
+//                               .ref('accounts/{userId}/pro-membership/token')
+//                               .onWrite( event => {
+//                                 console.log(event)
+//                               })  
 
-  const userModel: UserModel = {
-    displayName: user.displayName ? user.displayName : '',
-    email: user.email ? user.email : '',
-    uid: user.uid,
-    imagePath : user.photoURL? user.photoURL : 'https://avatars.dicebear.com/api/gridy/' + user.uid + '.svg',
-  }
-  return admin.firestore().collection('accounts').doc(user.uid).set(userModel)
 
-});
+
+
+
